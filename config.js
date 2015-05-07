0b522184758766546e1fde8f717d79242b967eca
Marcus Rosti <mer3ef@virginia.edu>
2015-05-06 23:55:38 -0400

    everything works from here

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://marcus-rosti.github.io/lightfare/',
+        url: 'http://my-ghost-blog.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -22,7 +22,7 @@ config = {
 
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '10.0.0.18',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         }
@@ -32,22 +32,20 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blogs published URL.
-        url: 'http://localhost:2368',
+        url: 'http://10.0.0.18:2368',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
+          mail: {
+              transport: 'SMTP',
+              options: {
+                  service: 'Gmail',
+                  auth: {
+                      user: 'lightfare.ghost@gmail.com', // mailgun username
+                      pass: 'salemsoccer'  // mailgun password
+                  }
+              }
+          },
 
         database: {
             client: 'sqlite3',
@@ -58,7 +56,7 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '10.0.0.18',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         },
@@ -73,7 +71,7 @@ config = {
     // Used when developing Ghost to run tests and check the health of Ghost
     // Uses a different port number
     testing: {
-        url: 'http://127.0.0.1:2369',
+        url: 'http://10.0.0.18:2369',
         database: {
             client: 'sqlite3',
             connection: {
@@ -81,7 +79,7 @@ config = {
             }
         },
         server: {
-            host: '127.0.0.1',
+            host: '10.0.0.18',
             port: '2369'
         },
         logging: false
@@ -90,11 +88,11 @@ config = {
     // ### Testing MySQL
     // Used by Travis - Automated testing run through GitHub
     'testing-mysql': {
-        url: 'http://127.0.0.1:2369',
+        url: 'http://10.0.0.18:2369',
         database: {
             client: 'mysql',
             connection: {
-                host     : '127.0.0.1',
+                host     : '10.0.0.18',
                 user     : 'root',
                 password : '',
                 database : 'ghost_testing',
@@ -102,7 +100,7 @@ config = {
             }
         },
         server: {
-            host: '127.0.0.1',
+            host: '10.0.0.18',
             port: '2369'
         },
         logging: false
@@ -111,11 +109,11 @@ config = {
     // ### Testing pg
     // Used by Travis - Automated testing run through GitHub
     'testing-pg': {
-        url: 'http://127.0.0.1:2369',
+        url: 'http://10.0.0.18:2369',
         database: {
             client: 'pg',
             connection: {
-                host     : '127.0.0.1',
+                host     : '10.0.0.18',
                 user     : 'postgres',
                 password : '',
                 database : 'ghost_testing',
@@ -123,7 +121,7 @@ config = {
             }
         },
         server: {
-            host: '127.0.0.1',
+            host: '10.0.0.18',
             port: '2369'
         },
         logging: false
